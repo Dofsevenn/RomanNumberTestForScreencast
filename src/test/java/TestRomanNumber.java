33279a02f08243092aae7eef9f9ffadf27d1b1bd
Kjetil <kjetil.dir@gmail.com>
2019-09-10 10:54:25 +0200

    Test 1

--- src/test/java/TestRomanNumber.java
+++ src/test/java/TestRomanNumber.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestRomanNumber {
@@ -7,6 +8,10 @@ public class TestRomanNumber {
     @Test
     void shouldConvert1ToI() {
         assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int numberInput) {
+        return null;
     }
 
 }
